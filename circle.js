b7d3233f01323e96b17ef6b80c330571a03ace98
“Marijus84” <marijus.romanovas@hotmail.com>
2022-09-26 19:33:59 +0300

    add task

--- circle.js
+++ circle.js
@@ -29,3 +29,18 @@ export default name;
 import getMeters from "./feetToM.js";
 //named import
 import { first, second } from "./feetToM.js";
+
+//todo Task
+/*
+Susikurkit naują projektą. Pridėkit script.js failą nurodydami, kad tipas 'module';
+Susikurkit po atskirą modulį valiutos konversijai: toGBP, toPLN, toUSD;
+Kiekvienas iš tų trijų modulių turi turėti const su EURto<currency> kursu(susiraskit kursą nete) 
+ir funkciją, kuri priima kiekį, ir grąžina suma kita valiuta; 
+Modulių funkcijos turi būti eksportuotos, o EURto<currency> konstanta ne; 
+Pasibandykite ir default ir named export;
+script.js susiimportuokit funkcijas ir patestuokite su visom konversijom; 
+
+Bonus. 
+Perkelkit tris konversijos modulius į vieną ir pasidarykit visus konversijų funkcijų
+exportus į named. Atitinkamai pakeiskit import script.js faile. 
+*/
